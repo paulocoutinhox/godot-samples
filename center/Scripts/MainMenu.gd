e9594e3bfbe4b670eaff655fd58334d949b07b24
Paulo Coutinho <paulo@prsolucoes.com>
2020-03-14 01:49:26 -0300

    project center updated

--- center/Scripts/MainMenu.gd
+++ center/Scripts/MainMenu.gd
@@ -1,36 +1,33 @@
 extends Node2D
 
 onready var background = $Background
-
 onready var screen_center = get_viewport_rect().size * 0.5
-onready var background_aspect = get_aspect_ratio(background.get_rect().size)
+onready var background_size = background.get_rect().size * background.scale
+onready var background_aspect = get_aspect_ratio(background_size)
 
 func _ready():
-  get_tree().get_root().connect("size_changed", self, "on_size_change")
-
-func on_size_change():
-  var view_size = get_viewport_rect().size
-  recenter_game(view_size)
-  resize_background(view_size)
-
-func recenter_game(view_size):
-  var new_center = view_size * 0.5
-  position = new_center - screen_center
-  
-func resize_background(view_size):
-  var new_aspect = get_aspect_ratio(view_size)
-
-  if new_aspect > background_aspect:
-	  background.get_rect().size.x = view_size.x
-	  background.get_rect().size.y = view_size.x / background_aspect
-  else:
-	  background.get_rect().size.y = view_size.y
-	  background.get_rect().size.x = view_size.y * background_aspect
+	get_tree().get_root().connect("size_changed", self, "on_size_change")
+	on_size_change()
 	
-  var new_center = view_size * 0.5
-  var new_size_half = background.get_rect().size * 0.5
-
-  background.position = screen_center - new_size_half
-  
+func on_size_change():
+	var view_size = get_viewport_rect().size
+	resize_background(view_size)
+	
+func resize_background(view_size):
+	var new_aspect = get_aspect_ratio(view_size)
+	var new_scale = 0.5
+	
+	if new_aspect > background_aspect:
+		new_scale = view_size.x / background_size.x
+	else:
+		new_scale = view_size.y / background_size.y
+	
+	# to be perfect can be something like 0.7
+		
+	background.set_scale(Vector2(new_scale, new_scale))
+	
 func get_aspect_ratio(size):
-  return size.x / size.y
+	return size.x / size.y
+	
+func _on_ButtonChange_pressed():
+	get_tree().change_scene("res://Scenes/Stage.tscn")
